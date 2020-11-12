f114c575edf3227086f502d69ad30d5e78736f6a
Barry Moore <chiroptical@gmail.com>
2020-11-12 11:16:01 -0500

    Release v0.2.2

--- default.nix
+++ default.nix
@@ -28,8 +28,8 @@ pkgs.stdenv.mkDerivation {
   name = "pytorch-introduction";
   buildInputs = inputs;
   src = builtins.fetchurl {
-    url = "https://github.com/chiroptical/pytorch-introduction/archive/v0.2.1.tar.gz";
-    sha256 = "09hhrv2ijgnbz53zz107wv80w31lyc76skmi92ry8bg6d701g496";
+    url = "https://github.com/chiroptical/pytorch-introduction/archive/v0.2.2.tar.gz";
+    sha256 = "1qmj9vih2s9vkpr2z896svay0cxjla3miwlq2l41sa0wzypfvfz4";
   };
   buildPhase = ''
     source $stdenv/setup
